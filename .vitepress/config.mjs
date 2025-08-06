fe61aa26af3d9022bf70c672316829c0e0cba1bf
unknown <1435473347@qq.com>
2025-08-06 18:01:17 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "天天的网站",
+  description: "天天制作网站",
+    themeConfig: {
+      logo: '/logo.png',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '示例', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: '演示',
+        items: [
+          { text: 'Markdown 示例', link: '/markdown-examples' },
+            { text: 'Runtime API 示例', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      ],
+      footer: {
+          copyright: 'Copyright © 2024-present 天天',
+      }
+    }
+})
